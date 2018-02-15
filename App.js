64993a5bdc2d5a4dd4c5a24fb5cdbcb8a457ef90
zaturnvalley <zacharycmason@gmail.com>
2018-02-14 18:25:23 -0800

    added key id to card

--- App.js
+++ App.js
@@ -22,6 +22,7 @@ export default class App extends React.Component {
   renderCard(item) {
     return (
       <Card
+        key={item.id}
         title={item.text}
         image={{ uri: item.uri }}
       >
