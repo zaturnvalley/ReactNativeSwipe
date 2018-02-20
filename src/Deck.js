ba85f33db4c92a7cf613ba8e4cbb1e0171438ff0
zaturnvalley <zacharycmason@gmail.com>
2018-02-20 08:47:22 -0800

    inputrange now width of screen

--- src/Deck.js
+++ src/Deck.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import { 
   View, 
   Animated,
-  PanResponder 
+  PanResponder,
+  Dimensions 
 } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class Deck extends Component {
   constructor(props) {
@@ -25,7 +28,7 @@ class Deck extends Component {
   getCardStyle() {
     const { position } = this.state;
     const rotate = position.x.interpolate({
-      inputRange: [-500, 0, 500],
+      inputRange: [-SCREEN_WIDTH, 0, SCREEN_WIDTH],
       outputRange: ['-120deg', '0deg', '120deg']
     });
 
